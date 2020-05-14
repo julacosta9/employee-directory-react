f0a52488178d3c289223799d48b008a99d52b4fe
Julian Acosta <julacosta9@gmail.com>
2020-05-13 23:17:05 -0700

    add sort button to table header

--- src/components/TableHead/index.js
+++ src/components/TableHead/index.js
@@ -1,19 +1,37 @@
 import React from "react";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 
 function TableHead(props) {
     return (
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Profile Picture</th>
+                <th>
+                    ID
+                    <Button name="id" onClick={props.handleSort}>
+                        Sort
+                    </Button>
+                </th>
+                <th>
+                    Profile Picture
+                </th>
                 <th>
                     First Name
-                    <Button onClick={props.handleSort}>Sort</Button>
+                    <Button name="firstName" onClick={props.handleSort}>
+                        Sort
+                    </Button>
                 </th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Username</th>
+                <th>
+                    Last Name
+                    <Button name="lastName" onClick={props.handleSort}>Sort</Button>
+                </th>
+                <th>
+                    Email
+                    <Button name="email" onClick={props.handleSort}>Sort</Button>
+                </th>
+                <th>
+                    Username
+                    <Button name="username" onClick={props.handleSort}>Sort</Button>
+                </th>
             </tr>
         </thead>
     );
