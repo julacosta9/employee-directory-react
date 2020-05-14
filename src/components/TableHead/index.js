7d7cbd6503fd1b40af36a770759db13374b182fb
Julian Acosta <8892386+julacosta9@users.noreply.github.com>
2020-05-13 19:48:03 -0700

    add sort button and update header titles

--- src/components/TableHead/index.js
+++ src/components/TableHead/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Button from "react-bootstrap/Button"
 
-function TableHead() {
+function TableHead(props) {
     return (
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Username</th>
+                <th>Profile Picture</th>
+                <th>
+                    First Name
+                    <Button onClick={props.handleSort}>Sort</Button>
+                </th>
+                <th>Last Name</th>
                 <th>Email</th>
-                <th>Phone</th>
+                <th>Username</th>
             </tr>
         </thead>
     );
